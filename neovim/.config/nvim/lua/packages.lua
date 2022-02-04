08e050ed0419bf770071c9a1fa084cecefab2f7f
Gabe Venberg <gabevenberg@gmail.com>
2022-02-04 14:10:05 -0600

    added friendly snippets

--- neovim/.config/nvim/lua/packages.lua
+++ neovim/.config/nvim/lua/packages.lua
@@ -34,6 +34,18 @@ return require('packer').startup(function(use)
 		}
 	}
 
+			-- 'honza/vim-snippets',
+	use {'L3MON4D3/LuaSnip',
+		requires={
+			'rafamadriz/friendly-snippets'
+		},
+		config=function()
+			require("luasnip.loaders.from_snipmate").load()
+			require("luasnip.loaders.from_vscode").load()
+		end
+
+	}
+
 	use {'nvim-telescope/telescope.nvim',
 		requires = {
 			{'nvim-lua/plenary.nvim'},
