ecab2689305cef8f2673d8b3f70f2d78381299a6
Gabe Venberg <gabevenberg@gmail.com>
2022-02-07 13:47:57 -0600

    re-added friendly-snippets without messing keybinds.

--- neovim/.config/nvim/lua/packages.lua
+++ neovim/.config/nvim/lua/packages.lua
@@ -34,6 +34,17 @@ return require('packer').startup(function(use)
 		}
 	}
 
+	use {'L3MON4D3/LuaSnip',
+		requires={
+			'rafamadriz/friendly-snippets'
+		},
+		config=function()
+			require("luasnip.loaders.from_snipmate").load()
+			require("luasnip.loaders.from_vscode").load()
+		end
+
+	}
+
 	use {'nvim-telescope/telescope.nvim',
 		requires = {
 			{'nvim-lua/plenary.nvim'},
