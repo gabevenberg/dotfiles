2a1fde4cbe06ee727552e5627ddce360846a85c2
Gabe Venberg <gabevenberg@gmail.com>
2022-03-10 12:21:11 -0600

    added angular language server config.

--- neovim/.config/nvim/lua/LSPconfig.lua
+++ neovim/.config/nvim/lua/LSPconfig.lua
@@ -49,6 +49,17 @@ vim.g.markdown_fenced_languages = {
   "ts=typescript"
 }
 
+--angular configuration. To install LS, $ npm install -g @angular/language-server
+local project_library_path = "/path/to/project/lib"
+local cmd = {"ngserver", "--stdio", "--tsProbeLocations", project_library_path , "--ngProbeLocations", project_library_path}
+
+require'lspconfig'.angularls.setup{
+  cmd = cmd,
+  on_new_config = function(new_config,new_root_dir)
+    new_config.cmd = cmd
+  end,
+}
+
 --rust configuration
 local rustOpts = {
     tools = { -- rust-tools options
