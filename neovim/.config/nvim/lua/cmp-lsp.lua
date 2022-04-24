1796fdbbf97eda3bcdca060f78683d4dace9f1c8
Gabe Venberg <gabevenberg@gmail.com>
2022-04-23 20:11:37 -0500

    removed custom cmp lsp setup, didnt play well with lspinstall.

--- neovim/.config/nvim/lua/cmp-lsp.lua
+++ neovim/.config/nvim/lua/cmp-lsp.lua
@@ -1,21 +1,6 @@
 --configure nvim-cmp for use with lsp and luasnip.
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
 local luasnip = require 'luasnip'
-
-local lspconfig = require('lspconfig')
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = {'texlab', 'rust_analyzer', 'pyright'}
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		-- on_attach = my_custom_on_attach,
-		capabilities = capabilities,
-	}
-end
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt='menu,menuone,preview,noinsert'
