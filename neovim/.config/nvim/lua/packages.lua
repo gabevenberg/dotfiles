453993aee647f3b14277176b132972eed4d03cf2
Gabe Venberg <gabevenberg@gmail.com>
2023-09-12 18:28:53 -0500

    ajusted toggleterm keybinds.

--- neovim/.config/nvim/lua/packages.lua
+++ neovim/.config/nvim/lua/packages.lua
@@ -151,14 +151,14 @@ return require('packer').startup(function(use)
 		config = function()
 			require("trouble").setup {
 				position = "bottom", -- position of the list can be: bottom, top, left, right
-				height = 10, -- height of the trouble list when position is top or bottom
-				width = 50, -- width of the list when position is left or right
-				icons = true, -- use devicons for filenames
+				height = 10,        -- height of the trouble list when position is top or bottom
+				width = 50,         -- width of the list when position is left or right
+				icons = true,       -- use devicons for filenames
 				mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-				fold_open = "", -- icon used for open folds
+				fold_open = "",  -- icon used for open folds
 				fold_closed = "", -- icon used for closed folds
-				group = true, -- group results by file
-				padding = true, -- add an extra new line on top of the list
+				group = true,       -- group results by file
+				padding = true,     -- add an extra new line on top of the list
 				action_keys = {
 					-- key mappings for actions in the trouble list
 					-- map to {} to remove a mapping, for example:
@@ -366,6 +366,7 @@ return require('packer').startup(function(use)
 	use { "akinsho/toggleterm.nvim", tag = '*', config = function()
 		require("toggleterm").setup {
 			insert_mappings = false,
+			terminal_mappings = false,
 			open_mapping = [[<c-\>]],
 		}
 	end }
